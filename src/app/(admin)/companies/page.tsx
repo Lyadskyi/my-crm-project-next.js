7d7b902c3a311a774e879353ffb39162776da872
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-11-03 21:27:05 +0200

    Added a layout for to create a markup template

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -22,11 +22,38 @@ export default function Page({}: PageProps) {
           <CompanyRow
             id={1}
             category="Products"
-            company="Costco"
+            company="Costco Wholesale"
+            status={Status.Active}
+            promotion={true}
+            country="USA"
+            joinedDate="2023.02.19"
+          />
+          <CompanyRow
+            id={2}
+            category="Products"
+            company="Costco Wholesale"
+            status={Status.NotActive}
+            promotion={false}
+            country="USA"
+            joinedDate="2023.02.19"
+          />
+          <CompanyRow
+            id={3}
+            category="Products"
+            company="Costco Wholesale"
             status={Status.Pending}
             promotion={true}
             country="USA"
-            joinedDate="02.19.2023"
+            joinedDate="2023.02.19"
+          />
+          <CompanyRow
+            id={4}
+            category="Products"
+            company="Costco Wholesale"
+            status={Status.Suspended}
+            promotion={false}
+            country="USA"
+            joinedDate="2023.02.19"
           />
         </CompanyTable>
       </main>
