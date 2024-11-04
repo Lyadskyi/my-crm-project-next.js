19b036e2ac9e3b6f2e9ffaf7cfd6b24444aac19c
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-11-04 23:28:24 +0200

    Created several different paths (routes) for the same page or component

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import MagicButton from '@/app/components/magic-button';
 
 export interface PageProps {}
 
@@ -7,7 +6,6 @@ export default function Page({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import AddCompanyButton from '@/app/components/add-company-button';
-import MagicButton from '@/app/components/magic-button';
-
-// import StatusLabel, { Status } from '@/app/components/status-label';
-// import ServerComponent from '@/app/components/server-component';
-// import ClientComponent from '@/app/components/client-component';
-// import ServerComponentCopy from '@/app/components/server-component-copy';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-      <MagicButton />
     </main>
   );
 }
