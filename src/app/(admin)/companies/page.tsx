4e98e5dedc48310a5838564ad9ea01a1000a550f
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-11-05 22:34:37 +0200

    Added interception of routes in the project

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import SearchInput from '@/app/components/search-input';
-import AddCompanyButton from '@/app/components/add-company-button';
 import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
@@ -13,86 +9,80 @@ export interface PageProps {}
 export default function Page({}: PageProps) {
   return (
     <>
-      <Header>Companies</Header>
-      <main>
-        <Toolbar action={<AddCompanyButton />}>
-          <SearchInput />
-        </Toolbar>
-        <CompanyTable>
-          <CompanyRow
-            id={1}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Active}
-            promotion={true}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={2}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.NotActive}
-            promotion={false}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={3}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Pending}
-            promotion={true}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={4}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Suspended}
-            promotion={false}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={5}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Active}
-            promotion={true}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={6}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.NotActive}
-            promotion={true}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={7}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Pending}
-            promotion={false}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-          <CompanyRow
-            id={8}
-            category="Products"
-            company="Costco Wholesale"
-            status={Status.Suspended}
-            promotion={true}
-            country="USA"
-            joinedDate="2023.02.19"
-          />
-        </CompanyTable>
-      </main>
+      <CompanyTable>
+        <CompanyRow
+          id={1}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Active}
+          promotion={true}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={2}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.NotActive}
+          promotion={false}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={3}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Pending}
+          promotion={true}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={4}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Suspended}
+          promotion={false}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={5}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Active}
+          promotion={true}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={6}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.NotActive}
+          promotion={true}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={7}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Pending}
+          promotion={false}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+        <CompanyRow
+          id={8}
+          category="Products"
+          company="Costco Wholesale"
+          status={Status.Suspended}
+          promotion={true}
+          country="USA"
+          joinedDate="2023.02.19"
+        />
+      </CompanyTable>
     </>
   );
 }
